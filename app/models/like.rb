6ab3a48a3065c89ee3e99bc21816dfecdbcff15c
Raghu Betina <raghu@firstdraft.com>
2020-02-05 12:49:17 +0000

    Direct association between posts and likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,8 @@
 class Like < ApplicationRecord
   # Direct associations
 
+  belongs_to :post
+
   # Indirect associations
 
   # Validations
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Post, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:likes) }
+
     end
 
     describe "InDirect Associations" do
