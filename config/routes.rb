67cd17d40b2f8956a811981c9a04e6310f60bbbf
Raghu Betina <raghu@firstdraft.com>
2020-02-05 12:49:29 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "posts#index"
   # Routes for the Following resource:
 
